cdab4d5033c6b1de9e296cd57f8da265f32bbc79
zacanger <zac@zacanger.com>
2019-02-01 09:52:25 -0700

    Address PR review

--- bin/cli.js
+++ bin/cli.js
@@ -2,14 +2,13 @@
 
 'use strict'
 
-const TerminalLauncher = require('./lib/TerminalLauncher')
+const TerminalLauncher = require('../lib/TerminalLauncher')
 const scriptPath = process.argv[2]
 const {resolve} = require('path')
 
 if (scriptPath) {
   const path = resolve(scriptPath)
   TerminalLauncher.launchTerminal({path}).catch(err => {
-    console.log(err)
-    throw err
+    console.error(err)
   })
 }
